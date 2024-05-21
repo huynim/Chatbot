a33c1fe7166f455f51e313076f744fa671b480ac
huynim <huy@post.com>
2024-05-21 12:59:30 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,11 +5,11 @@ import os
 from datetime import datetime
 
 st.set_page_config(
-    page_title="FSH",
-    page_icon="🐟",
+    page_title="Home",
+    page_icon="👋",
 )
 
-st.sidebar.success(" ")
+st.sidebar.success("Select a demo above.")
 
 # Ensure the 'data' directory exists
 os.makedirs('data', exist_ok=True)
@@ -38,8 +38,10 @@ async def generate_pdf(url, pdf_path):
         await page.pdf(path=pdf_path, format='A4')
         
         await browser.close()
+        
+        return pdf_path
 
-st.text_input("Lenke til nettsted:", key="url", placeholder="Skriv inn lenken")
+st.text_input("URL", key="url", placeholder="Skriv inn lenken")
 
 if 'generate_pdf_button' not in st.session_state:
     st.session_state.generate_pdf_button = False
@@ -47,13 +49,13 @@ if 'generate_pdf_button' not in st.session_state:
 def set_button_flag():
     st.session_state.generate_pdf_button = True
 
-st.button("Fortsett", on_click=set_button_flag)
+st.button("Generate PDF", on_click=set_button_flag)
 
 if st.session_state.generate_pdf_button:
     url = st.session_state.url
     if url:
         st.write("Generating PDF...")
-        asyncio.run(generate_pdf(url, 'document.pdf'))
+        pdf_path = asyncio.run(generate_pdf(url, 'document.pdf'))
         st.write(f"PDF generated: {pdf_path}")
     else:
         st.write("Please enter a URL.")
